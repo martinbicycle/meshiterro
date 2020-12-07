6f3ce867f2c9777a1553fffe67c9db42c995e908
martinbicycle <martinbicycle7700@gmail.com>
2020-12-07 05:08:44 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+# Refile.secret_key = '51321513c3619b7366f7ea4a840394cea825a8acee8955b100861fd5a80e08f5b6d5b62744b2a66918a7ff353c021f66de5bb7beb6428bcfef3b59a22ca05d81'
